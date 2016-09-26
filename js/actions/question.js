509ea241ee45c8bf092f13681b07200ed6a29749
azad <azad26195@gmail.com>
2016-09-26 17:11:51 +0530

    submitAnswer and setQuestionIndex action added in questionPage

---
+++ js/actions/question.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import type {Action} from './types';
+
+export const SUBMIT_ANSWER = "SUBMIT_ANSWER";
+export const SET_QUESTION_INDEX = "SET_QUESTION_INDEX";
+
+export function submitAnswer(submitedAnswer:any, passProps:any):Action {
+	return {
+		type: SUBMIT_ANSWER,
+		passProps: passProps,
+		submitedAnswer: submitedAnswer
+	}
+}
+
+export function setQuestionIndex(index:number):Action {
+	return {
+		type: SET_QUESTION_INDEX,
+		selectedQuestionIndex: index
+	}
+}
