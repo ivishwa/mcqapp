e9a39d1e74733a24a786bac66fe8bb232ec9aba3
azad <azad26195@gmail.com>
2016-10-02 22:38:29 +0530

    Blankpage2 added in sidebar

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -30,6 +30,9 @@ class SideBar extends Component {
                     <ListItem button onPress={() => this.navigateTo('blankPage')} >
                         <Text>Blank Page</Text>
                     </ListItem>
+                    <ListItem button onPress={() => this.navigateTo('blankPage2')} >
+                        <Text>Blank Page</Text>
+                    </ListItem>
                 </List>
             </Content>
         );
