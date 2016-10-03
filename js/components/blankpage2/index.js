ef80c957bf07c3418287dc6f51376b3eae669812
azad <azad26195@gmail.com>
2016-10-03 18:21:21 +0530

    hello world tested of shouten ui

--- js/components/blankpage2/index.js
+++ js/components/blankpage2/index.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  Examples
 } from '@shoutem/ui';
 import { connect } from 'react-redux';
 	
 class BlankPage2 extends Component {
 	render(){
 		return (
-			<View>
-				<Text>{{"Shoutem UI Test"}}</Text>
-			</View>
+			<Examples />
 		)
 	}
 }
