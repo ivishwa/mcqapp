d81dd71af9fc741ed2246a0ec79de14f562e7b97
azad <azad26195@gmail.com>
2016-10-04 15:48:30 +0530

    chore(actions): rehydrate added to questions

--- js/reducers/question.js
+++ js/reducers/question.js
@@ -3,22 +3,29 @@
 import type {Action} from '../actions/types';
 import { SET_QUESTION_INDEX } from '../actions/question';
 import { QUESTION_PAPER } from '../test_data/questions';
+import { REHYDRATE } from 'redux-persist/constants'
+
 
 export type State = {
-    list: any
-}
+    questions: any
+} 
 
 const initialState = {
-    selectedQuestionIndex: 0,
-    currentQuestion: undefined,
-};
-
+    questions: undefined
+}
 export default function (state:State = initialState, action:Action): State {
     if (action.type === SET_QUESTION_INDEX) {
         return {
+            ...state,
             selectedQuestionIndex: action.payload,
-            currentQuestion: state.list
-        };
+          };
+    }
+
+    if (action.type === REHYDRATE) {
+        const savedData = action['payload']['selectedQuestionIndex'] || state;
+        return {
+            ...savedData
+        }
     }
     return state;
 }
