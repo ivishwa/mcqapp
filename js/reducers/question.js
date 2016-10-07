cd81495940d6660a81c26b4c4278be181e37b155
azad <azad26195@gmail.com>
2016-10-07 21:04:16 +0530

    chore(reducer): SUBMIT_ANSWER reducer added

--- js/reducers/question.js
+++ js/reducers/question.js
@@ -1,17 +1,19 @@
 'use strict';
 
 import type {Action} from '../actions/types';
-import { SET_QUESTION_INDEX } from '../actions/question';
+import { SET_QUESTION_INDEX, SUBMIT_ANSWER} from '../actions/question';
 import { QUESTION_PAPER } from '../test_data/questions';
 import { REHYDRATE } from 'redux-persist/constants'
 
 
 export type State = {
-    questions: any
-} 
+    questions: any,
+    answers: any
+}
 
 const initialState = {
-    questions: undefined
+    questions: undefined,
+    answers: {}
 }
 export default function (state:State = initialState, action:Action): State {
     if (action.type === SET_QUESTION_INDEX) {
@@ -19,6 +21,16 @@ export default function (state:State = initialState, action:Action): State {
             ...state,
             selectedQuestionIndex: action.payload,
           };
+    }
+
+    if (action.type === SUBMIT_ANSWER) {
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
+          [ state.selectedQuestionIndex ]: action.submitedAnswer
+        }
+      }
     }
 
     if (action.type === REHYDRATE) {
