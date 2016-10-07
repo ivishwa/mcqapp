bbb67e0757885bf914586b469b43892642b27d77
azad <azad26195@gmail.com>
2016-10-07 17:35:07 +0530

    shoutem/ui removed

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -30,9 +30,6 @@ class SideBar extends Component {
                     <ListItem button onPress={() => this.navigateTo('blankPage')} >
                         <Text>Blank Page</Text>
                     </ListItem>
-                    <ListItem button onPress={() => this.navigateTo('blankPage2')} >
-                        <Text>Shoutem Page</Text>
-                    </ListItem>
                 </List>
             </Content>
         );
