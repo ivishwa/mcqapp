439b9208d1e82860531104906c9c1f6b43257376
azad <azad26195@gmail.com>
2016-10-07 21:01:38 +0530

    fix(reducder): question reducer use val

--- js/actions/question.js
+++ js/actions/question.js
@@ -5,11 +5,11 @@ import type {Action} from './types';
 export const SUBMIT_ANSWER = "SUBMIT_ANSWER";
 export const SET_QUESTION_INDEX = "SET_QUESTION_INDEX";
 
-export function submitAnswer(submitedAnswer:any, passProps:any):Action {
+export function submitAnswer(val:any, passProps:any):Action {
 	return {
 		type: SUBMIT_ANSWER,
 		passProps: passProps,
-		submitedAnswer: submitedAnswer
+		submitedAnswer: val
 	}
 }
 
